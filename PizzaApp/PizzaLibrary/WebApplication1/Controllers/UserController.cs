5c7f244ef301496fe0ee802a0c32e3236e101dd1
Eric Agocs <ago.er@yandex.com>
2018-07-12 12:08:04 -0400

    added models and created homepage

---
+++ PizzaApp/PizzaLibrary/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        //new
+        public IActionResult Users()
+        {
+            ViewData["Message"] = "Users list.";
+
+            return View();
+        }
+
+        //new
+        public IActionResult Orders()
+        {
+            ViewData["Message"] = "Orders list.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
---
+++ PizzaApp/PizzaLibrary/WebApplication1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    // this is going to be our context model.
+    // but we will write it.
+    // should be a "POCO" "plain old CLR object"
+    // need parameter-less contructor (usually default)
+    // and public get-set properties
+    public class User
+    {
+        //hidden property
+        public int Id { get; set; }
+        [Required] //cant submit username without a firstname
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
